fb5a9231e49f4f999d172802c72412d191c145e2
Bui Quang Vinh <buivinh4979@gmail.com>
2024-08-08 21:09:55 +0700

    setup Projected Route and Rejected Route

--- shopeeClone-FE/src/useRouterElement.tsx
+++ shopeeClone-FE/src/useRouterElement.tsx
@@ -1,31 +1,59 @@
-import { useRoutes } from 'react-router-dom'
+/* eslint-disable react-refresh/only-export-components */
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 // import ProductList from './Pages/ProductList'
 import Register from './Pages/Register'
 import Login from './Pages/Login'
 import RegisterLayout from './Layouts/RegisterLayout'
-import Tailwind from './Pages/Taildwindcss'
+import Profile from './Pages/Profile'
+import MainLayout from './Layouts/MainLayout'
+
+const isAuthenticate = false
+function ProtectedRoute() {
+  return isAuthenticate ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectRoute() {
+  return isAuthenticate ? <Navigate to='/' /> : <Outlet />
+}
 
 export default function useRouterElement() {
   const routeElement = useRoutes([
     {
       path: '/',
-      element: <Tailwind />
+      element: <MainLayout />,
+      index: true
     },
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: <Profile />
+        }
+      ]
     },
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectRoute />,
+      children: [
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
   return routeElement
